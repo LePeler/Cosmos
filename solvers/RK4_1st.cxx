7ac847132c036cf4318a7853d27894cd888e75d4
Fabian Aurora Wohlfahrt (any/all) <fabian.wohlfahrt03@gmail.com>
2025-08-21 18:02:08 +0200

    minor code cleanup

--- solvers/RK4_1st.cxx
+++ solvers/RK4_1st.cxx
@@ -4,10 +4,10 @@
 double RK4_1st::ComputeStep(double dt) const {
     // make an RK4 step
 
-    double k1 = RK4_1st::func(y, t);
-    double k2 = RK4_1st::func(y + k1*dt/2, t + dt/2);
-    double k3 = RK4_1st::func(y + k2*dt/2, t + dt/2);
-    double k4 = RK4_1st::func(y + k3*dt, t + dt);
+    double k1 = func(y, t);
+    double k2 = func(y + k1*dt/2, t + dt/2);
+    double k3 = func(y + k2*dt/2, t + dt/2);
+    double k4 = func(y + k3*dt, t + dt);
 
     return y + (k1 + 2*k2 + 2*k3 + k4)/6 * dt;
 }
